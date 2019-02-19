a0c6002a0f34a4c302e9e69c37c4b1b28debff4a
shraddhas8 <47772998+shraddhas8@users.noreply.github.com>
2019-02-19 17:44:57 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int arr[]={2,3,7,5,1};
+    printf("Array Created");
+    for(i=5;i>=0;i--)
+    {
+        printf(" %d ",arr[i]);
+    }
+}
